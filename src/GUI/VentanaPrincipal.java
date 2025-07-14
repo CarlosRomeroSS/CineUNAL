c2c016388652421181696d6e354a4743b6cc5e78
CarlosRomeroSS <cromerosi@unal.edu.co>
2025-07-14 14:49:48 -0500

    Ejemplo Básico de VentanaPrincipal.java
    EDITAR

--- src/GUI/VentanaPrincipal.java
+++ src/GUI/VentanaPrincipal.java
@@ -1,1 +1,19 @@
+import javax.swing.*;
 
+public class VentanaPrincipal extends JFrame {
+    public VentanaPrincipal() {
+        setTitle("CineUNAL");
+        setSize(400, 300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JButton boton = new JButton("Reservar");
+        add(boton);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new VentanaPrincipal();
+    }
+}
