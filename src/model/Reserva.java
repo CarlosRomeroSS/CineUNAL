b90751f31864fb32a5f14fa72695d3708ddafe32
Tomás García <togarcial@unal.edu.co>
2025-07-14 20:54:11 -0500

    Se instancia la clase Reserva, se añaden sus atributos y el constructor

--- src/model/Reserva.java
+++ src/model/Reserva.java
@@ -1,1 +1,19 @@
+import java.util.ArrayList;
 
+public class Reserva {
+    private integer id;
+    private Cliente cliente;
+    private Funcion funcion;
+    private List<Asiento> asientos;
+    private String fecha;
+    private double total;
+
+    public Reserva (integer id, Cliente cliente, Funcion funcion, String fecha){
+        this.id = id;
+        this.cliente = cliente;
+        this.funcion = funcion;
+        this.asientos = new ArrayList<>();
+        this.fecha = fecha;
+        this.total = 0;
+    }
+}
