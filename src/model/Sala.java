215b05a563b4de5f911fd13862425784c44430f6
LuisaRochaJ <marochaj@unal.edu.co>
2025-07-17 15:57:20 -0500

    Update Sala.java

--- src/model/Sala.java
+++ src/model/Sala.java
@@ -1,4 +1,8 @@
-public class Sala{
+public class Sala extends Asiento{
+  private int filas;
+  private int columnas;
+  private String pelicula;
+  private String horario
   public Sala(String nompeli, int filas, int columnas){
   }
 }
