98c27c53974f2f97a9ff07b065d6dd5ed3b72732
LuisaRochaJ <marochaj@unal.edu.co>
2025-07-21 22:19:50 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("¡Bienvenido a CineUNAL!");
     
-    //toca inicializar las salas que ya deberiamos tener pensada
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.ArrayList;
-
+    }
+}
 
 
 
