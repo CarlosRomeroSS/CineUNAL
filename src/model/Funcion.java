bece009c38d45ca8cf7bf5b56df38a7f49dc8fbd
LuisaRochaJ <marochaj@unal.edu.co>
2025-07-21 22:04:23 -0500

    Update Funcion.java

--- src/model/Funcion.java
+++ src/model/Funcion.java
@@ -1,1 +1,45 @@
 
+// Funcion.java
+public class Funcion {
+    private Integer id;
+    private LocalDateTime fechaHora;
+    private Sala sala;
+    private Pelicula pelicula;
+    private String tipoVersion;
+    
+    // Constructor
+    public Funcion(Integer id, LocalDateTime fechaHora, Sala sala, Pelicula pelicula, String tipoVersion) {
+        this.id = id;
+        this.fechaHora = fechaHora;
+        this.sala = sala;
+        this.pelicula = pelicula;
+        this.tipoVersion = tipoVersion;
+    }
+    
+    // Getters y Setters
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    
+    public LocalDateTime getFechaHora() { return fechaHora; }
+    public void setFechaHora(LocalDateTime fechaHora) { this.fechaHora = fechaHora; }
+    
+    public Sala getSala() { return sala; }
+    public void setSala(Sala sala) { this.sala = sala; }
+    
+    public Pelicula getPelicula() { return pelicula; }
+    public void setPelicula(Pelicula pelicula) { this.pelicula = pelicula; }
+    
+    public String getTipoVersion() { return tipoVersion; }
+    public void setTipoVersion(String tipoVersion) { this.tipoVersion = tipoVersion; }
+    
+    // Método específico del diagrama
+    public Map<Asiento, AsientoEstado> mostrarDisponibilidad() {
+        Map<Asiento, AsientoEstado> disponibilidad = new HashMap<>();
+        
+        for (Asiento asiento : sala.getDistribucion().values()) {
+            disponibilidad.put(asiento, asiento.getEstado());
+        }
+        
+        return disponibilidad;
+    }
+}
