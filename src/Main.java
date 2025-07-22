347e6982891b84239971170ecc4aefbf5374b85e
LuisaRochaJ <marochaj@unal.edu.co>
2025-07-21 22:25:19 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,14 @@ import java.util.HashMap;
 import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("¡Bienvenido a CineUNAL!");
-    
-    }
+        Administrador admin = new Administrador();
+        
+        Pelicula pelicula = new Pelicula(1, "Avatar", "Ciencia Ficción", 180, 
+            "PG-13", "Una historia épica...", "James Cameron");
+        admin.agregarPelicula(pelicula);
+        
+        System.out.println("¡Sistema funcionando!");
+    }
 }
 
 
