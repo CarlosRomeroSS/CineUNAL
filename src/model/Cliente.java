a522c27f76e48a233bfac2bd17a95c790e6406e6
LuisaRochaJ <marochaj@unal.edu.co>
2025-07-21 22:03:16 -0500

    Update Cliente.java

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,1 +1,36 @@
+import java.time.DateTime;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.math.BigDecimal;
 
+public class Cliente {
+    private Integer id;
+    private String nombre;
+    private String email;
+    private ClienteTipo tipoCliente;
+    public Cliente(Integer id, String nombre, String email, ClienteTipo tipoCliente) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.tipoCliente = tipoCliente;
+    }
+    
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    public ClienteTipo getTipoCliente() { return tipoCliente; }
+    public void setTipoCliente(ClienteTipo tipoCliente) { this.tipoCliente = tipoCliente; }
+    
+    public Reserva crearReserva() {
+        return null;
+    }
+}
