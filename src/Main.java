cdb58c6cc4ac9ca94da5d22d9790c6c937ea2f50
LuisaRochaJ <marochaj@unal.edu.co>
2025-07-21 21:57:53 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,59 +6,15 @@ public class Main {
 
     }
 }
-
+import java.time.DateTime;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.math.BigDecimal;
 
 // Sala.java
-public class Sala {
-    private Integer id;
-    private String nombre;
-    private SalaTipo tipo;
-    private Integer capacidad;
-    private Map<Integer, Asiento> distribucion;
-    
-    // Constructor
-    public Sala(Integer id, String nombre, SalaTipo tipo, Integer capacidad) {
-        this.id = id;
-        this.nombre = nombre;
-        this.tipo = tipo;
-        this.capacidad = capacidad;
-        this.distribucion = new HashMap<>();
-    }
-    
-    // Getters y Setters
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
-    
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    
-    public SalaTipo getTipo() { return tipo; }
-    public void setTipo(SalaTipo tipo) { this.tipo = tipo; }
-    
-    public Integer getCapacidad() { return capacidad; }
-    public void setCapacidad(Integer capacidad) { this.capacidad = capacidad; }
-    
-    public Map<Integer, Asiento> getDistribucion() { return distribucion; }
-    public void setDistribucion(Map<Integer, Asiento> distribucion) { this.distribucion = distribucion; }
-    
-    // Método específico del diagrama
-    public void configurarSala() {
-        // Configurar asientos según el tipo de sala
-        distribucion.clear();
-        
-        String[] filas = {"A", "B", "C", "D", "E", "F", "G", "H"};
-        int asientosPorFila = capacidad / filas.length;
-        int asientoId = 1;
-        
-        for (String fila : filas) {
-            for (int num = 1; num <= asientosPorFila; num++) {
-                Asiento asiento = new Asiento(asientoId, fila, num);
-                distribucion.put(asientoId, asiento);
-                asientoId++;
-            }
-        }
-    }
-}
 
 // Cliente.java
 public class Cliente {
